d5a4850e49275e08cf613630f8abd96a7e166533
evencoding <88191233+evencoding@users.noreply.github.com>
2021-11-11 02:50:58 +0900

    MovieAPI with GraphQL

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
-import resolvers from "./graphql/resolvers";
+import resolvers from "./graphql/resolvers.js";
 
 const server = new GraphQLServer({
-  typeDefs: "graphql/schema.graphql",
+  typeDefs: "graphql/scheme.graphql",
   resolvers,
 });
 
-server.start(() => console.log("GraphQLServer Running"));
+server.start(() => console.log("Graphql Server Running"));
